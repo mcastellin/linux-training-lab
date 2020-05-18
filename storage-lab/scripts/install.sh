802a9ad3e31068872a22a7667384d5632d4c346b
Manuel Castellin <manuel@castellinconsulting.com>
2020-05-18 11:42:01 +0100

    chore: enable firewalld for all labs
    All machines in labs must have Firewalld service enalbed. This forces us to
    configure to practice configuring firewalls on every lab

--- storage-lab/scripts/install.sh
+++ storage-lab/scripts/install.sh
@@ -5,3 +5,9 @@ sudo yum install -q -y \
     vim \
     telnet \
     wget
+
+# Enable firewall for machines in this lab
+systemctl list-units --type=service --state=active | grep firewalld.service > /dev/null
+if [ $? -ne 0 ]; then
+    systemctl enable firewalld.service --now 2>&1
+fi
